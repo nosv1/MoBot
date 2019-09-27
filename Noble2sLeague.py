a23c1e765f796d8101fd36d04a889e2e051c9106
Christopher Seagraves <christopher.seagraves@users.noreply.github.com>
2019-09-27 07:48:36 -0500

    Update Noble2sLeague.py

--- Noble2sLeague.py
+++ Noble2sLeague.py
@@ -85,7 +85,8 @@ async def main(args, message, client):
       await tCaptain(message, tCommandLog)
         
   if (args[0].lower() == "!registerid" or args[0].lower() == "!changeid"):
-          await registerID(message, args)
+    if (" RL " not in message.content):
+        await registerID(message, args)
 
   if (args[0] == "!minor" or args[0] == "!major"):
     if (args[1].lower() == "checkin"):
