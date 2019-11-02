735faea89a48d922667a630ffed31b16b6820fcd
Christopher Seagraves <christopher.seagraves@users.noreply.github.com>
2019-11-02 11:41:01 -0500

    Update MoBotLoop.py

--- MoBotLoop.py
+++ MoBotLoop.py
@@ -234,7 +234,6 @@ async def checkTEGarrettPointApplications(nowPacificTime, client):
   if (logUpdated):
     pointApplicationsLogSheet.update_cells(pointApplicationsLogRange, value_input_option="USER_ENTERED")
     pointApplicationsLogSheet.resize(rows=pointApplicationsLogSheet.row_count + 1)
-    print("Log Updated")
 
 async def updateMoBotStatus(client):
   # changing bot status based on rand gen
