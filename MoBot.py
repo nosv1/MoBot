5b5d510bc1cb9b47496e2d5ca842f5f2e475be76
Christopher Seagraves <christopher.seagraves@users.noreply.github.com>
2019-11-21 20:05:36 -0600

    Update MoBot.py

--- MoBot.py
+++ MoBot.py
@@ -472,7 +472,7 @@ async def on_message(message):
         break
 
     # check for clock accuracy...
-    if (random.random() < .01):
+    if (random.random() < .005):
       await ClocksAndCountdowns.checkClockAccuracy(client)
 
   except:
