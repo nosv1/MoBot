1757c9ea93b22a3437806f0b1d1533ba35456483
Christopher Seagraves <christopher.seagraves@users.noreply.github.com>
2020-01-28 13:47:46 -0600

    Update Noble2sLeague.py

--- Noble2sLeague.py
+++ Noble2sLeague.py
@@ -684,7 +684,7 @@ async def saveNRT(message, member):
             except: # incase forbidden or something
               pass
 
-          await message.guild.get_channel(REGISTER_LOG_CHNL).send("%s has saved their NRT with:\nID: `%s`\nPlatform: `%s`" % (member.mention, playerID, platform))
+          await message.guild.get_channel(REGISTER_LOG_CHNL).send("%s has saved their NRT with:\nID: `%s`\nPlatform: `%s`\nNRT: `%s`" % (member.mention, playerID, platform, nrt))
 
         else:
           await message.channel.send("**Cannot Save NRT**\n%s, there is another player saved under your Discord ID.\nID: `%s`\nPlatform: `%s`\nTracker: <%s>" % (member.mention, r[i+3].value, r[i+2].value, trackerURL), delete_after=10)
