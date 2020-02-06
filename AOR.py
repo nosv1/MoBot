8622ff8bb3ab266720b5049c672d06c7c9a30d93
Christopher Seagraves <christopher.seagraves@users.noreply.github.com>
2020-02-06 17:36:17 -0600

    Update AOR.py

--- AOR.py
+++ AOR.py
@@ -556,7 +556,7 @@ async def updateStandings(client):
     moBotMember = message.guild.get_member(moBot)
     embed.color = moBotMember.roles[-1].color
 
-    print("\nUpdating AOR Standings\n")
+    print("\nUpdating AOR Standings")
     await messageEmbed[0].edit(content=spaceChar, embed=messageEmbed[1])
 # end updateStandings
 
