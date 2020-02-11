aed3cb5de921fe878cf1d33bd962f68715abc420
Christopher Seagraves <christopher.seagraves@users.noreply.github.com>
2020-02-11 10:44:14 -0600

    Update ClocksAndCountdowns.py

--- ClocksAndCountdowns.py
+++ ClocksAndCountdowns.py
@@ -53,7 +53,7 @@ async def main(args, message, client):
     await prepareEditor(message, args[2], await createChannel(message, args[1]), 1, 0)
   elif len(args) > 3:
     if "countdown" in args[1]:
-      await shorthand(message, args[1])
+      await shorthand(message, args[2])
 # end main
 
 async def memberJoin(member):
