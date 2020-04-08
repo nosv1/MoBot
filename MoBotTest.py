2500158357e3e3824f4d1cfc503d55c1c5c4b7c3
Christopher Seagraves <christopher.seagraves@users.noreply.github.com>
2020-04-08 09:41:51 -0500

    Update MoBotTest.py

--- MoBotTest.py
+++ MoBotTest.py
@@ -144,7 +144,7 @@ async def on_message(message):
       
     if (len(args) > 1):
       if (args[1] == "test"):
-        await Noble2sLeague.sendNRT(message, args)
+        await Imperial.sendDSN(message, args)
         await message.channel.send("done", delete_after=3)
       elif (args[1] == "table"):
         await MoBotTables.main(args, message, client)
