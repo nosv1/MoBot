4a22d2873171effa61147cbf324ddb6e6616634b
Christopher Seagraves <christopher.seagraves@users.noreply.github.com>
2020-06-07 12:18:04 -0500

    Update COTM.py

--- COTM.py
+++ COTM.py
@@ -1181,6 +1181,9 @@ async def handleReserveReaction(message, payload, member):
   updateReservesSpreadsheet(message.guild, after_reserve_combos)
   await updateReserveRoles(message.guild, before_reserve_combos, after_reserve_combos)
   await updateReserveEmbed(message, after_reserves, after_reserve_combos)
+
+  if t_need > -1:
+    await updateStartOrderEmbed(message.guild, member_divs[-1])
 # end handleReserveReaction
 
 
