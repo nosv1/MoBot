c287b406765aa4ef8fe89218e2f14552cf9faff3
Chris Seagraves <christopher.seagraves@gmail.com>
2020-07-02 23:53:17 -0500

    Update MoBotTest.py

--- MoBotTest.py
+++ MoBotTest.py
@@ -147,7 +147,7 @@ async def on_message(message):
       
     if (len(args) > 1):
       if (args[1] == "test"):
-        await ClocksAndCountdowns.createClock(message)
+        await message.author.add_roles(None)
         await message.channel.send("done", delete_after=3)
       elif (args[1] == "table"):
         await MoBotTables.main(args, message, client)
