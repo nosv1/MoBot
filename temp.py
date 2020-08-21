32b3d6b910b3723d16b6ab9bf6149398fb425c90
Chris Seagraves <christopher.seagraves@gmail.com>
2020-08-21 13:49:19 -0500

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,7 +1,11 @@
-import subprocess
+from requests_html import HTMLSession
 
-def foo():
-  p = subprocess.Popen(["python", "D:/Users/Nick/OneDrive/GTA Stuff/Bots/MoBot/MoBot/MoBot.py"], shell=True, stdout=subprocess.PIPE)
-  p = subprocess.Popen(["python", "D:/Users/Nick/OneDrive/GTA Stuff/Bots/MoBot/MoBot/MoBotLoop.py"], shell=True, stdout=subprocess.PIPE)
+def getVehicleImage():
+  session = HTMLSession()
+  url = f"https://gta.fandom.com/wiki/{'Gauntlet Classic Custom'.replace(' ', '_')}"
+  r = session.get(url)
+  image_url = r.html.html.split("pi-image-thumbnail")[0].split("image image-thumbnail")[-1].split("src=\"")[1].split("\"")[0]
+  return {"wiki_url" : url, "image_url" : image_url}
+# end getVehicleImage
 
-foo()
+print(getVehicleImage())
